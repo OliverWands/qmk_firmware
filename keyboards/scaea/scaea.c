71f44c645c6bb118787dbe9f74083dbe7ef7d30c
OliverWands <wandschneider.oliver@gmail.com>
2020-10-04 13:05:10 +0200

    Code formatting

--- keyboards/scaea/scaea.c
+++ keyboards/scaea/scaea.c
@@ -2,117 +2,116 @@
 
 void startSequence(void) {
     setPinOutput(L1);
-	setPinOutput(L2);
-	setPinOutput(L3);
+    setPinOutput(L2);
+    setPinOutput(L3);
 
-    for (int inx = 0; inx < 7; inx++)
-	{
-		writePinHigh(L1);
-		wait_us(50000);
-		writePinLow(L1);
-		writePinHigh(L2);
-		wait_us(50000);
-		writePinLow(L2);
-		writePinHigh(L3);
-		wait_us(50000);
-		writePinLow(L3);
-	}
+    for (int inx = 0; inx < 7; inx++) {
+        writePinHigh(L1);
+        wait_us(50000);
+        writePinLow(L1);
+        writePinHigh(L2);
+        wait_us(50000);
+        writePinLow(L2);
+        writePinHigh(L3);
+        wait_us(50000);
+        writePinLow(L3);
+    }
 
-	// writePinHigh(L1);
-	// wait_us(150000);
-	// writePinLow(L1);
-	// writePinHigh(L2);
-	// wait_us(150000);
-	// writePinLow(L2);
-	// writePinHigh(L3);
-	// wait_us(150000);
-	// writePinLow(L3);
+    // writePinHigh(L1);
+    // wait_us(150000);
+    // writePinLow(L1);
+    // writePinHigh(L2);
+    // wait_us(150000);
+    // writePinLow(L2);
+    // writePinHigh(L3);
+    // wait_us(150000);
+    // writePinLow(L3);
 
-	// writePinHigh(L1);
-	// wait_us(125000);
-	// writePinLow(L1);
-	// writePinHigh(L2);
-	// wait_us(125000);
-	// writePinLow(L2);
-	// writePinHigh(L3);
-	// wait_us(125000);
-	// writePinLow(L3);
+    // writePinHigh(L1);
+    // wait_us(125000);
+    // writePinLow(L1);
+    // writePinHigh(L2);
+    // wait_us(125000);
+    // writePinLow(L2);
+    // writePinHigh(L3);
+    // wait_us(125000);
+    // writePinLow(L3);
 
-	// writePinHigh(L1);
-	// wait_us(100000);
-	// writePinLow(L1);
-	// writePinHigh(L2);
-	// wait_us(100000);
-	// writePinLow(L2);
-	// writePinHigh(L3);
-	// wait_us(100000);
-	// writePinLow(L3);
+    // writePinHigh(L1);
+    // wait_us(100000);
+    // writePinLow(L1);
+    // writePinHigh(L2);
+    // wait_us(100000);
+    // writePinLow(L2);
+    // writePinHigh(L3);
+    // wait_us(100000);
+    // writePinLow(L3);
 
-	// writePinHigh(L1);
-	// wait_us(75000);
-	// writePinLow(L1);
-	// writePinHigh(L2);
-	// wait_us(75000);
-	// writePinLow(L2);
-	// writePinHigh(L3);
-	// wait_us(75000);
-	// writePinLow(L3);
+    // writePinHigh(L1);
+    // wait_us(75000);
+    // writePinLow(L1);
+    // writePinHigh(L2);
+    // wait_us(75000);
+    // writePinLow(L2);
+    // writePinHigh(L3);
+    // wait_us(75000);
+    // writePinLow(L3);
 
-	// for (int inx = 0; inx < 5; inx++)
-	// {
-	// 	writePinHigh(L1);
-	// 	wait_us(50000);
-	// 	writePinLow(L1);
-	// 	writePinHigh(L2);
-	// 	wait_us(50000);
-	// 	writePinLow(L2);
-	// 	writePinHigh(L3);
-	// 	wait_us(50000);
-	// 	writePinLow(L3);
-	// }
+    // for (int inx = 0; inx < 5; inx++)
+    // {
+    // 	writePinHigh(L1);
+    // 	wait_us(50000);
+    // 	writePinLow(L1);
+    // 	writePinHigh(L2);
+    // 	wait_us(50000);
+    // 	writePinLow(L2);
+    // 	writePinHigh(L3);
+    // 	wait_us(50000);
+    // 	writePinLow(L3);
+    // }
 
-	// for (int inx = 0; inx < 10; inx++)
-	// {
-	// 	writePinHigh(L1);
-	// 	wait_us(25000);
-	// 	writePinLow(L1);
-	// 	writePinHigh(L2);
-	// 	wait_us(25000);
-	// 	writePinLow(L2);
-	// 	writePinHigh(L3);
-	// 	wait_us(25000);
-	// 	writePinLow(L3);
-	// }
+    // for (int inx = 0; inx < 10; inx++)
+    // {
+    // 	writePinHigh(L1);
+    // 	wait_us(25000);
+    // 	writePinLow(L1);
+    // 	writePinHigh(L2);
+    // 	wait_us(25000);
+    // 	writePinLow(L2);
+    // 	writePinHigh(L3);
+    // 	wait_us(25000);
+    // 	writePinLow(L3);
+    // }
 
-	// for (int inx = 0; inx < 15; inx++)
-	// {
-	// 	writePinHigh(L1);
-	// 	wait_us(12500);
-	// 	writePinLow(L1);
-	// 	writePinHigh(L2);
-	// 	wait_us(12500);
-	// 	writePinLow(L2);
-	// 	writePinHigh(L3);
-	// 	wait_us(12500);
-	// 	writePinLow(L3);
-	// }
+    // for (int inx = 0; inx < 15; inx++)
+    // {
+    // 	writePinHigh(L1);
+    // 	wait_us(12500);
+    // 	writePinLow(L1);
+    // 	writePinHigh(L2);
+    // 	wait_us(12500);
+    // 	writePinLow(L2);
+    // 	writePinHigh(L3);
+    // 	wait_us(12500);
+    // 	writePinLow(L3);
+    // }
 
-	// for (int inx = 0; inx < 20; inx++)
-	// {
-	// 	writePinHigh(L1);
-	// 	wait_us(6250);
-	// 	writePinLow(L1);
-	// 	writePinHigh(L2);
-	// 	wait_us(6250);
-	// 	writePinLow(L2);
-	// 	writePinHigh(L3);
-	// 	wait_us(6250);
-	// 	writePinLow(L3);
-	// }
+    // for (int inx = 0; inx < 20; inx++)
+    // {
+    // 	writePinHigh(L1);
+    // 	wait_us(6250);
+    // 	writePinLow(L1);
+    // 	writePinHigh(L2);
+    // 	wait_us(6250);
+    // 	writePinLow(L2);
+    // 	writePinHigh(L3);
+    // 	wait_us(6250);
+    // 	writePinLow(L3);
+    // }
 
-	writePinLow(L1);
-  	writePinLow(L2);
-  	writePinLow(L3);
+    writePinLow(L1);
+    writePinLow(L2);
+    writePinLow(L3);
 }
 
 layer_state_t setLayerIndication(layer_state_t state, bool binaryOut) {
